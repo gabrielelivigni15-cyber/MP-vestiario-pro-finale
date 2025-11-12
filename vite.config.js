df6a66f262917a04c21166c6b75e531e2e3c8b1c
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-11-12 11:14:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // ✅ fondamentale per far funzionare su Vercel
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
