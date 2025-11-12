d9c724e2e5a38adacba5a299b32110b70ec663ae
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-11-12 11:18:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ fondamentale per far funzionare su Vercel
+  base: "./", // ⚠️ fondamentale — non "/"
   build: {
     outDir: "dist",
     assetsDir: "assets",
+  },
+  server: {
+    port: 5173,
+    host: true,
   },
 });
